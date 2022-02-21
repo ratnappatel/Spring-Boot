4c0ea2d1e29dcbb764ba1598fa2783c7c448d7cf
ratnappatel <ratnappatel@gmail.com>
2022-02-21 11:25:03 +0530

    Spring Boot Security Custom Login Form

---
+++ boot-form-handling/src/main/java/com/gl/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.gl.service;
+
+import java.util.List;
+
+import com.gl.model.Employee;
+
+public interface EmployeeService {
+	void insertEmployee(Employee emp);
+	void insertEmployees(List<Employee> employees);
+	List<Employee> getAllEmployees();
+	void getEmployeeById(String empid);
+}
